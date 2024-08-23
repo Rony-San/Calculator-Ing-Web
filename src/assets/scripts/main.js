c59bbfe8750a6b2ba6498179bd4a59b285b07960
Rony-san <rony.banol@udea.edu.co>
2024-08-23 04:57:05 -0500

    Initial commit ✨

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,105 @@
+// Seleccionar elementos del DOM
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+
+let displayValue = "";
+let currentValue = "";
+let previousValue = "";
+let operator = null;
+
+function updateDisplay(value) {
+  display.value = value;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+
+    if (button.classList.contains("operator")) {
+      handleOperator(buttonValue);
+    } else if (button.classList.contains("clear")) {
+      clearDisplay();
+    } else {
+      handleNumber(buttonValue);
+    }
+  });
+});
+
+function handleNumber(value) {
+  if (value === "." && currentValue.includes(".")) return;
+  currentValue += value;
+  displayValue += value;
+  updateDisplay(displayValue);
+}
+
+function handleOperator(value) {
+  if (value === "=") {
+    calculateResult();
+  } else {
+    if (currentValue === "" && previousValue !== "") {
+      operator = value;
+      displayValue = displayValue.slice(0, -1) + value;
+      updateDisplay(displayValue);
+      return;
+    }
+    if (operator === null) {
+      previousValue = currentValue;
+    } else {
+      calculateResult(false); // Calcula el resultado sin limpiar el display
+    }
+    operator = value;
+    displayValue += value;
+    updateDisplay(displayValue);
+    currentValue = "";
+  }
+}
+
+// Función para realizar el cálculo
+function calculateResult(finalCalculation = true) {
+  let result = 0;
+  const prev = parseFloat(previousValue);
+  const curr = parseFloat(currentValue);
+
+  if (isNaN(prev) || isNaN(curr)) return;
+
+  switch (operator) {
+    case "+":
+      result = prev + curr;
+      break;
+    case "-":
+      result = prev - curr;
+      break;
+    case "*":
+      result = prev * curr;
+      break;
+    case "/":
+      result = prev / curr;
+      break;
+    default:
+      return;
+  }
+
+  currentValue = result.toString();
+  previousValue = currentValue;
+
+  if (finalCalculation) {
+    displayValue = currentValue;
+  } else {
+    displayValue += currentValue;
+  }
+
+  operator = null;
+  updateDisplay(displayValue);
+}
+
+// Función para limpiar la pantalla y resetear la calculadora
+function clearDisplay() {
+  displayValue = "";
+  currentValue = "";
+  previousValue = "";
+  operator = null;
+  updateDisplay("0");
+}
+
+// Inicializar la pantalla con 0
+clearDisplay();
